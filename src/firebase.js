66b548e3d0a257167295ff39b50749c8a166afda
Rohit Chanda <rohitchanda00713@gmail.com>
2026-04-08 21:07:22 +0530

    chore: extract firebase secrets to .env file

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlEx84N2KWYCTENe7rXtDKJKblWBNd20s",
-  authDomain: "project-b7c46.firebaseapp.com",
-  projectId: "project-b7c46",
-  storageBucket: "project-b7c46.firebasestorage.app",
-  messagingSenderId: "857669598949",
-  appId: "1:857669598949:web:2589a913f28ef13a311e0c",
-  measurementId: "G-WPTC3ERYJS"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
